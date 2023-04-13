32094954cd7a35ef79901b7a74c874e40b1dd07b
imransarkerweb <imransarker.web@gmail.com>
2023-04-13 13:32:41 +0600

    firebase google popup and signout implemented

---
+++ firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwWaZRn2gT_GNDZLsiiIj6vMqiGbzgcW4",
+  authDomain: "simple-firbase-a38a1.firebaseapp.com",
+  projectId: "simple-firbase-a38a1",
+  storageBucket: "simple-firbase-a38a1.appspot.com",
+  messagingSenderId: "766474845342",
+  appId: "1:766474845342:web:1cecf822162387fda80f8f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,18 +1,52 @@
-import React from "react";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from "react";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../../../firebase.init";
 
 const Login = () => {
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
 
   const handleSignPopUp = () => {
-    console.log("Google mama is comming");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const currentUser = result.user;
+        console.log(currentUser);
+        setUser(currentUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
   };
   return (
     <div>
       <p>Hi there!</p>
-      <button onClick={handleSignPopUp}>Google login</button>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleSignPopUp}>Google login</button>
+      )}
+      {user && (
+        <>
+          <img src={user.photoURL} alt="" />
+          <h2>User: {user.displayName}</h2>
+          <h3>Email: {user.email}</h3>
+        </>
+      )}
     </div>
   );
 };
